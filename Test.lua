ea813f1672a5d550f3d0e1a21fcaac78711ad4b1
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-01-15 09:04:49 +0700

    Update

--- Test.lua
+++
@@ -1,3 +0,0 @@
--- Takoyaki X - Universal
-local TakoyakiXLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Takoyaki/master/Ui%20Takoyaki.lua"))()
-local TakoyakiX = TakoyakiXLibrary.new("Takoyaki X", 0)
